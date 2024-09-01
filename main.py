7af502ac5f5b77f3497b298717f7898fae3f17e3
GCerchioni70 <62259138+GCerchioni70@users.noreply.github.com>
2024-09-01 14:00:29 +0200

    Add Akhinh to manage delete on comments

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Lesson 69 from Udemy Course
+# Added: use of git to connect to Git Hub Remote Repository
+# Added: ### You can allow the user who commented to delete the comment.
+# Added: you can add time(showing days, hours, mins and just now) to the comment
+# from  Akhinh
+
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
@@ -12,19 +18,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -104,6 +97,22 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
 
 
+# Create a Comment table for users that have commented
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(600), nullable=False)
+    posted_time = db.Column(db.DateTime, nullable=False)
+
+    # ***************Child Relationship for User*************#
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment_author = relationship("User", back_populates="comments")
+
+    # ***************Child Relationship for BlogPost*************#
+    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
+    parent_post = relationship("BlogPost", back_populates="comments")
+
+
 with app.app_context():
     db.create_all()
 
@@ -119,6 +128,38 @@ def admin_only(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
+
+def only_commenter(function):
+    @wraps(function)
+    def check(*args, **kwargs):
+        user = db.session.execute(db.select(Comment).where(Comment.author_id == current_user.id)).scalar()
+        if not current_user.is_authenticated or current_user.id != user.author_id:
+            return abort(403)
+        return function(*args, **kwargs)
+    return check
+
+# This is the function calculating the days, hours, mins and just now
+def calculate_time_difference(posted_time):
+    current_time = datetime.now()
+    time_difference = current_time - posted_time
+
+    # Extract days, hours, and minutes
+    days = time_difference.days
+
+     # the divmod divide the time_difference.seconds by 3600 and then save the answer as a tuple, with the whole number and remainder,
+      # as hours and remainder(minutes) respectively. the same as the second tuple, only that the remainder( _ ) which is the seconds is not needed.
+    hours, remainder = divmod(time_difference.seconds, 3600)
+    minutes, _ = divmod(remainder, 60)
+
+    if days > 0:
+        return f"{days} days ago"
+    elif hours > 0:
+        return f"{hours} hours ago"
+    elif minutes > 0:
+        return f"{minutes} minutes ago"
+    else:
+        return "just now"
 
 
 # Register new users into the User database
@@ -190,25 +231,50 @@ def get_all_posts():
 
 
 # Add a POST method to be able to post comments
+# additional code to the show_post route(the show_post function)
 @app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
     # Add the CommentForm to the route
-    comment_form = CommentForm()
-    # Only allow logged-in users to comment on posts
-    if comment_form.validate_on_submit():
+    form = CommentForm()
+    if form.validate_on_submit():
+        # if not login, dont allow to comment
         if not current_user.is_authenticated:
-            flash("You need to login or register to comment.")
+            flash("You will need to login to comment.")
             return redirect(url_for("login"))
-
         new_comment = Comment(
-            text=comment_form.comment_text.data,
-            comment_author=current_user,
-            parent_post=requested_post
+            text=form.comment.data,
+            author_id=current_user.id,
+            post_id=requested_post.id,
+            posted_time=datetime.now()
         )
         db.session.add(new_comment)
         db.session.commit()
-    return render_template("post.html", post=requested_post, current_user=current_user, form=comment_form)
+
+        # tracing the days, hours and minutes each comment is made
+        # list of comments with the same post_id
+    comments = Comment.query.filter_by(post_id=post_id).all()
+
+    # updating the time and turning it to list. note: this doesn't affect the data(time) in posted_time in the
+    # comment query
+    the_time = []
+    for comments_time in comments:
+        print(comments_time.posted_time)
+        time = calculate_time_difference(comments_time.posted_time)
+        the_time.append(time)
+    print(the_time)
+
+    return render_template("post.html", post=requested_post, form=form, logged_in=current_user, posted_time=the_time)
+
+
+# You can allow the user who commented to delete the comment.
+@app.route("/delete/comment/<int:comment_id>/<int:post_id>")
+@only_commenter
+def delete_comment(post_id, comment_id):
+    post_to_delete = db.get_or_404(Comment, comment_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('show_post', post_id=post_id))
 
 
 # Use a decorator so only an admin user can create new posts
